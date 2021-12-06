b26ed1fcc0cfc652691758913cd6eebf7c1b1084
ATP_429 <atpgaming123@gmail.com>
2021-12-06 12:38:19 +0530

    Create instruction menu

--- src/menu/InstructionMenu.java
+++ src/menu/InstructionMenu.java
@@ -1,13 +1,24 @@
 package menu;
 
+import java.awt.Graphics2D;
+
+import main.Camera;
 import main.Main;
+import texture.TextureManager;
 
 public class InstructionMenu extends Menu
 {
-
 	public InstructionMenu(Main game)
 	{
 		super(game);
 	}
 	
+	@Override
+	public void render(Camera cam, Graphics2D bg)
+	{
+		cam.translate(bg);
+		cam.drawImage(bg, TextureManager.get("instructions_menu").getImage(), 0, 0, Main.RENDER_WIDTH, Main.RENDER_HEIGHT);
+		cam.reset(bg);
+		super.render(cam, bg);
+	}
 }
